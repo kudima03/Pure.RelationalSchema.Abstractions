497f06f7d77a7cb1d66d58dbd5c02c08bf4fcacc
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-23 11:39:39 +0300

    Added IIndex.

---
+++ src/Pure.RelationalSchema.Abstractions/Index/IIndex.cs
@@ -0,0 +1,11 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.RelationalSchema.Abstractions.Column;
+
+namespace Pure.RelationalSchema.Abstractions.Index;
+
+public interface IIndex
+{
+    IBool IsUnique { get; }
+
+    IEnumerable<IColumn> Columns { get; }
+}
