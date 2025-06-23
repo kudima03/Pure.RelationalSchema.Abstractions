d7ba21bbcbf554dfd3213b6ff5a63d5e71cda463
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-23 11:40:36 +0300

    Added ISchema.

---
+++ src/Pure.RelationalSchema.Abstractions/Schema/ISchema.cs
@@ -0,0 +1,14 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.RelationalSchema.Abstractions.ForeignKey;
+using Pure.RelationalSchema.Abstractions.Table;
+
+namespace Pure.RelationalSchema.Abstractions.Schema;
+
+public interface ISchema
+{
+    IString Name { get; }
+
+    IEnumerable<ITable> Tables { get; }
+
+    IEnumerable<IForeignKey> ForeignKeys { get; }
+}
