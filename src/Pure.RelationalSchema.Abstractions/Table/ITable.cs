cef1e7d204e653a1cea33b16e46ee2c60f3caf4a
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-23 11:39:56 +0300

    Added ITable.

---
+++ src/Pure.RelationalSchema.Abstractions/Table/ITable.cs
@@ -0,0 +1,14 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.RelationalSchema.Abstractions.Column;
+using Pure.RelationalSchema.Abstractions.Index;
+
+namespace Pure.RelationalSchema.Abstractions.Table;
+
+public interface ITable
+{
+    IString Name { get; }
+
+    IEnumerable<IColumn> Columns { get; }
+
+    IEnumerable<IIndex> Indexes { get; }
+}
