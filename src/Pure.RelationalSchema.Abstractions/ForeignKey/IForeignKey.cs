dd367df6d861d3aa45092e0043e180672c44e161
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-02 20:47:32 +0300

    Code formatted according to new rules.

--- src/Pure.RelationalSchema.Abstractions/ForeignKey/IForeignKey.cs
+++ src/Pure.RelationalSchema.Abstractions/ForeignKey/IForeignKey.cs
@@ -1,15 +1,15 @@
-﻿using Pure.RelationalSchema.Abstractions.Column;
+using Pure.RelationalSchema.Abstractions.Column;
 using Pure.RelationalSchema.Abstractions.Table;
 
 namespace Pure.RelationalSchema.Abstractions.ForeignKey;
 
 public interface IForeignKey
 {
-    ITable ReferencingTable { get; }
+    public ITable ReferencingTable { get; }
 
-    IColumn ReferencingColumn { get; }
+    public IColumn ReferencingColumn { get; }
 
-    ITable ReferencedTable { get; }
+    public ITable ReferencedTable { get; }
 
-    IColumn ReferencedColumn { get; }
+    public IColumn ReferencedColumn { get; }
 }
